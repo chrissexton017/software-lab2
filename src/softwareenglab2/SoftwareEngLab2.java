a8ec8a55f4b90a734d3f1c359c8fd57840de55a9
Chris <Chris@Chris-PC>
2016-04-12 14:09:35 +0200

    Initialization of project

---
+++ src/softwareenglab2/SoftwareEngLab2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package softwareenglab2;
+
+/**
+ *
+ * @author Chris
+ */
+public class SoftwareEngLab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        printMessage();
+    }
+    
+    public static void printMessage() {
+        System.out.println("This lab is an enormous amount of fun");
+    }
+    
+}
